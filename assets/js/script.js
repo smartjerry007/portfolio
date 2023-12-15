8d18f64b3f8b6cc7ad6d6cc7f3156903c3c18109
smartjerry007 <smartjerrydev@gmail.com>
2023-12-15 06:45:53 +0000

    stack change

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,7 +72,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["a Full Stack developer", "an AI Developer", "a shopify developer"],
+    strings: ["a Full Stack developer","a shopify developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
